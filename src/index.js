2cf18a093ccd29fa62657346aa932590f628226b
Ewelina <ewelinazawol@gmail.com>
2023-01-23 22:38:23 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Ecommerce app listening on port ${port}`);
+});
